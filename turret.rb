b8da8118cb49f9aba23b70d372ad52b560de4b06
Jonathan Poole <jpoole@auvik.com>
2015-03-12 10:51:09 -0400

    Added a logfile

--- turret.rb
+++ turret.rb
@@ -4,12 +4,8 @@ set :public_folder, "public"
 set :views, "views"
 
 get '/' do
-      return 'Hello world'
-end
-
-
-class HelloWorldApp < Sinatra::Base
-  get '/:greeting/?:name?' do
-    "#{params[:greeting]}, #{params[:name] ? params[:name] : 'world'}!"
-  end
+  File.open(logfile.log, 'w') { |file|
+    file.write("#{params}")
+  }
+  puts "#{params}"
 end
